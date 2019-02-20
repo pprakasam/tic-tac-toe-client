0e50d85e7f8e740c4dc230414f239b05f37217f7
pprakasam <padmaprakasam@gmail.com>
2019-02-19 21:20:10 -0500

    Complete game logic
    Change game board display from html table rows and columns to bootstrap
    divs.
    
    Change javascript nested array based game logic to one dimensional
    array similar to api array
    
    Complete user authentication sign-up, sign-in, sign-out, change password
    
    Create game api, update game api and get game api

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,14 +6,15 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const events = require('./events.js')
+
 $(() => {
-  // your JS code goes here
-  $('table tr td').click(function () {
-    const x = $(this).parent().index()
-    const y = $(this).index()
-    const setVariable = events.startGame(x, y)
-    $(this).text(setVariable)
-    $(this).off('click')
+  $('.sign-up').submit(events.onSignUp)
+  $('.sign-in').submit(events.onSignIn)
+  $('.sign-out').submit(events.onSignOut)
+  $('.change-password').submit(events.onChangePassword)
+  $('.start-button').on('click', events.onNewGame)
+  $('.box').on('click', function () {
+    events.startGame(this.id)
   })
   $('.reset-button').submit(events.resetGame)
 })
